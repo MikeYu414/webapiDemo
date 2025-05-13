d58953f02ac1a16b2b068d809c36e2b354b79b72
MikeYu414 <66950479+MikeYu414@users.noreply.github.com>
2025-05-13 14:45:11 +0800

    Add files via upload

---
+++ WebApiDemo/WebApiDemo/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace WebApiDemo.Models
+{
+    public class TodoItem
+    {
+        public string? Url { get; set; }
+        public object? Content { get; set; }
+    }
+}
