cc511880a372abfb74ef88b3264b7a6e2cf8d16a
MikeYu414 <66950479+MikeYu414@users.noreply.github.com>
2025-05-13 14:03:24 +0800

    Add files via upload

---
+++ WebApiDemo/WebApiDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiDemo.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddHttpClient("ExternalApi", client =>
+{
+    client.BaseAddress = new Uri("https://apisandbox.iras.gov.sg/");
+    client.DefaultRequestHeaders.Add("Accept", "application/json");
+    client.DefaultRequestHeaders.Add("X-IBM-Client-Id", "9cd729c9161bc98b43db8d031e2bcad1");
+    client.DefaultRequestHeaders.Add("X-IBM-Client-Secret", "ca059de6fc014507dfac56d031cf5da7");
+    // 可以添加认证头等
+    // client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "token");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
