e6c41b3390aac77330464e41bffbc6ec7604af95
MikeYu414 <66950479+MikeYu414@users.noreply.github.com>
2025-05-14 15:50:17 +0800

    Add files via upload

---
+++ WebApiDemo/WebApiDemo/Controllers/CRMApiController.cs
@@ -0,0 +1,37 @@
+﻿using Ave.WebJob.Common;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Xrm.Sdk;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CRMApiController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CRMApiController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET: api/<CRMApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/<CRMApiController>
+        [HttpPost]
+        public void Post()
+        {
+            var client = new D365ConnectionService(_config).GetOrganizationService();
+            Entity entity = new Entity("new_webjob");
+            entity["new_name"] = DateTime.Now.ToString("s");
+            client.Create(entity);
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Dynamics365/D365ConnectionService.cs
@@ -0,0 +1,88 @@
+﻿/********************************************************************
+ *
+ *  PROPRIETARY and CONFIDENTIAL
+ *
+ *  This file is licensed from, and is a trade secret of:
+ *
+ *                   AvePoint, Inc.
+ *                   525 Washington Blvd, Suite 1400
+ *                   Jersey City, NJ 07310
+ *                   United States of America
+ *                   Telephone: +1-201-793-1111
+ *                   WWW: www.avepoint.com
+ *
+ *  Refer to your License Agreement for restrictions on use,
+ *  duplication, or disclosure.
+ *
+ *  RESTRICTED RIGHTS LEGEND
+ *
+ *  Use, duplication, or disclosure by the Government is
+ *  subject to restrictions as set forth in subdivision
+ *  (c)(1)(ii) of the Rights in Technical Data and Computer
+ *  Software clause at DFARS 252.227-7013 (Oct. 1988) and
+ *  FAR 52.227-19 (C) (June 1987).
+ *
+ *  Copyright © 2023 AvePoint® Inc. All Rights Reserved. 
+ *
+ *  Unpublished - All rights reserved under the copyright laws of the United States.
+ */
+using Microsoft.PowerPlatform.Dataverse.Client;
+
+namespace Ave.WebJob.Common
+{
+    public sealed class D365ConnectionService
+    {
+        private ServiceClient clientService;
+        private static readonly D365ConnectionService instance = null;
+        private Dictionary<string, ServiceClient> clientServiceDictionary;
+        private const int ConnectionTimeOutMinutes = 2;
+        private readonly IConfiguration _config;
+
+        public D365ConnectionService(IConfiguration config)
+        {
+            clientServiceDictionary = new Dictionary<string, ServiceClient>();
+            _config = config;
+        }
+
+        public static D365ConnectionService Instance
+        {
+            get { return instance; }
+        }
+
+        public ServiceClient GetOrganizationService()
+        {
+            string connectionString = _config["CRMConnectString"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("CRMConnectString");
+            }
+            if (!clientServiceDictionary.TryGetValue(connectionString, out clientService))
+            {
+                //ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                //ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => { return true; };
+                clientService = new ServiceClient(connectionString);
+                clientServiceDictionary.Add(connectionString, clientService);
+            }
+            return clientService;
+        }
+
+        public ServiceClient RenewOrganizationService(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("connectionString");
+            }
+
+            clientService = new ServiceClient(connectionString);
+            if (clientServiceDictionary.ContainsKey(connectionString))
+            {
+                clientServiceDictionary[connectionString] = clientService;
+            }
+            else
+            {
+                clientServiceDictionary.Add(connectionString, clientService);
+            }
+            return clientService;
+        }
+    }
+}
