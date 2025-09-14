cd9ae92480592b7d88d513476b49643bc962fade
mike.yu <mike.yu@avepoint.com>
2025-09-14 09:57:14 +0800

    1

---
+++ WebApiDemo/WebApiDemo/Models/JobSettingModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Batchjob.App.Models
+{
+    public class JobSettingModel
+    {
+        public Dictionary<string, string> ClassLogGroups { get; set; } = new Dictionary<string, string>();
+        public bool SwaggerEnabled { get; set; }
+        public int TimerInterval { get; set; }
+        public bool DebugMode { get; set; }
+        public int MaxConcurrentJobs { get; set; }
+
+    }
+}
